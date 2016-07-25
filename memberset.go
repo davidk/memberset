3053683eccd905f88c9af2fdbeb59d11544a2437
davidk <kdavid@gmail.com>
2016-07-25 10:05:12 -0700

    Clean up (lint)

--- memberset.go
+++ memberset.go
@@ -1,13 +1,17 @@
-/* Package memberset is a simple set used for testing memberships in Go.
+/*
+
+  Package memberset is a simple set used for testing memberships in Go.
 
   import "github.com/davidk/memberset"
 
   m := memberset.New()
 
   // Other types are also acceptable, like ints
+  
   m.Add("123")
 
   // Is string "123" a member?
+
   if ok := m.Get("123"); ok {
     // Yes
   } else {
@@ -15,27 +19,28 @@
   }
 
   // To remove "123" from the set
+
   m.Delete("123")
 
 */
-
 package memberset
 
 import (
   "sync"
 )
 
+// Memberset wraps up a map[interface{}]struct{} for ease of use.
 type MemberSet struct {
   sync.RWMutex
   m map[interface{}]struct{}
 }
 
-// New() initializes a new MemberSet
+// New initializes a new MemberSet
 func New() *MemberSet {
   return &MemberSet{ m: make(map[interface{}]struct{}) }
 }
 
-// Get() looks for an item in the set. It doesn't really retrieve
+// Get looks for an item in the set. It doesn't really retrieve
 // anything, just checks for existence.
 func (s *MemberSet) Get (value interface{}) (bool) {
   defer s.RUnlock()
@@ -53,14 +58,14 @@ func (s *MemberSet) Add (value interface{}) {
   s.Set(value)
 }
 
-// Set() adds the value to the set
+// Set adds a value to the set
 func (s *MemberSet) Set (value interface{}) {
   s.Lock()
   s.m[value] = struct{}{}
   s.Unlock()
 }
 
-// Delete() removes the value from the set
+// Delete removes the value from the set
 func (s *MemberSet) Delete (value interface{}) {
   s.Lock()
   delete(s.m, value)
